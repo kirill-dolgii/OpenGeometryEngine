c54e295ea74abdfa588a8b53f7aa1ac05a1e5665
peperonchinni <maindolgiykirill99@gmail.com>
2023-06-08 19:36:10 +0300

    Bounds.ContainsParam is implemented

--- Classes/Bounds.cs
+++ Classes/Bounds.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace OpenGeometryEngine;
 
 public readonly struct Bounds
@@ -6,4 +8,8 @@ public readonly struct Bounds
     public readonly double End;
 
     public Bounds(double start, double end) => (Start, End) = (start, end);
+
+	public bool ContainsParam(double param) =>
+		Math.Abs(param - Start) <= Constants.Tolerance &&
+		Math.Abs(param - End) <= Constants.Tolerance;
 }
