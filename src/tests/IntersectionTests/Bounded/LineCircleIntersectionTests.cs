4fa4fa3c008cd410b5d9272c8ea7395c98a318f8
WawyWaker <maindolgiykirill99@gmail.com>
2024-01-30 17:38:26 +0300

    -

--- src/tests/IntersectionTests/Bounded/LineCircleIntersectionTests.cs
+++ src/tests/IntersectionTests/Bounded/LineCircleIntersectionTests.cs
@@ -1,26 +1,26 @@
-﻿//using OpenGeometryEngine;
+﻿using OpenGeometryEngine;
 
-//namespace OpenGeometryEngineTests.IntersectionTests.Bounded;
+namespace OpenGeometryEngineTests.IntersectionTests.Bounded;
 
-//[TestFixture]
-//public class LineCircleIntersectionTests
-//{
-//    [Test]
-//    public void LINESEGMENT_INTERSECTS_ARC()
-//    {
-//        var lineSegment = new LineSegment(new Line(new Point(-0.026, 0.009, 0), new UnitVec(0.8, 0.6, 0)),
-//                                      new Interval(.0, 0.015));
-//        var circleFrame = new Frame(new Point(-0.022, 0.004, 0),
-//                                    new Vector(0.928476690885259, 0.371390676354104, 0),
-//                                    new Vector(-0.371390676354104, 0.928476690885259, 0),
-//                                    new Vector(0, 0, 1));
-//        var circleSegment = new Arc(circleFrame, 0.0107703296143, new Interval(.0, 1.89891622181));
+[TestFixture]
+public class LineCircleIntersectionTests
+{
+    [Test]
+    public void LINESEGMENT_INTERSECTS_ARC_COPLANAR_2_POINTS()
+    {
+        var lineSegment = new LineSegment(new Point(0.019, 0.039, 0), new Point(0.055, 0.01, 0));
+        
+        var circleFrame = new Frame(new Point(0.033, 0.023, 0), UnitVec.UnitX, UnitVec.UnitY, UnitVec.UnitZ);
+        var circleSegment = new Arc(new Point(0.033, 0.023, 0.0), 
+                                    new Point(0.042544442902576, 0.0101585199575957, 0.0), 
+                                    new Point(0.0195315665986117, 0.0316372045079625, 0.0), 
+                                    UnitVec.UnitZ);
 
-//        var ip = circleSegment.IntersectCurve(lineSegment).Single();
+        var ip = circleSegment.IntersectCurve(lineSegment).Single();
 
-//        Assert.That(Accuracy.EqualLengths(ip.FirstEvaluation.Param, 0.00886256313108));
-//        Assert.That(Accuracy.EqualAngles(0.899296566986, ip.SecondEvaluation.Param));
-//        Assert.That(ip.FirstEvaluation.Point,
-//            Is.EqualTo(new Point(-0.0189099494951335, 0.0143175378786498, 0)));
-//    }
-//}
+        Assert.That(Accuracy.EqualLengths(ip.FirstEvaluation.Param, 0.00886256313108));
+        Assert.That(Accuracy.EqualAngles(0.899296566986, ip.SecondEvaluation.Param));
+        Assert.That(ip.FirstEvaluation.Point,
+            Is.EqualTo(new Point(-0.0189099494951335, 0.0143175378786498, 0)));
+    }
+}
