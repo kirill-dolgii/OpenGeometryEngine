3d3f7005032e5c15c6f90c1996c8118cb10d9d40
WawyWaker <maindolgiykirill99@gmail.com>
2024-02-01 15:29:01 +0300

    Added IFlatRegion interface #1

---
+++ src/main/Interfaces/Curves/IFlatRegion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace OpenGeometryEngine;
+
+public interface IFlatRegion : IEnumerable<ITrimmedCurve>
+{
+    double Length { get; }
+
+    double Area { get; }
+
+    Plane Plane { get; }
+
+    IEnumerable<IFlatRegion> InnerRegions { get; }
+
+    int WindingNumber(Point point);
+
+    IFlatRegion Offset(double offset);
+
+    bool Contains(IFlatRegion other);
+
+    bool Intersects(IFlatRegion other);
+}
