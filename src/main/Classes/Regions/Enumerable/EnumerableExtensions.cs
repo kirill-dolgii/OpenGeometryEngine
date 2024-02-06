f10b0ff8713cc640dfab2b76887058c8bacd5d1e
Kirill Dolgii <maindolgiykirill99@gmail.com>
2024-02-06 16:13:35 +0300

    BreadthTraversal and Split are implemented in Graph

---
+++ src/main/Classes/Regions/Enumerable/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenGeometryEngine.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<Tuple<T, T>> Pairs<T> (this IEnumerable<T> items, bool closed = false)
+    {
+        Argument.IsNotNull(nameof(items), items);
+        using var enumerator = items.GetEnumerator();
+        if (!enumerator.MoveNext()) yield break;
+        var first = enumerator.Current;
+        while (enumerator.MoveNext())
+        {
+            yield return Tuple.Create(first, enumerator.Current);
+            first = enumerator.Current;
+        }
+        if (closed) yield return Tuple.Create(first, items.First());
+    }
+}
--- src/main/Collections/Graph.cs
+++ src/main/Collections/Graph.cs
@@ -44,18 +44,15 @@ public class Graph<TNode, TEdge>
     }
 
     public Graph(ICollection<TNode> nodes, Func<TNode, ICollection<TNode>> adjecencyFinder, 
-        Func<(TNode, TNode), ICollection<TEdge>> edgeFinder, bool directed) : this(directed)
+        Func<(TNode, TNode), TEdge> edgeFinder, bool directed) : this(directed)
     {
         foreach (var node in nodes)
         {
             var neighbours = adjecencyFinder(node);
             foreach (var neighbour in neighbours)
             {
-                var edges = edgeFinder.Invoke((node, neighbour));
-                foreach (var edge in edges)
-                {
-                    AddEdge(node, neighbour, edge);
-                }
+                var edge = edgeFinder.Invoke((node, neighbour));
+                AddEdge(node, neighbour, edge);
             }
         }
     }
@@ -119,16 +116,16 @@ public class Graph<TNode, TEdge>
         return true;
     }
 
-    public ICollection<(TNode, TNode, TEdge)> DepthTraversal(TNode startNode)
+    public ICollection<ValueTuple<TNode, TNode, TEdge>> DepthTraversal(TNode start)
     {
-        Argument.IsNotNull(nameof(startNode), startNode);
-        if (!ContainsNode(startNode)) throw new ArgumentException(nameof(startNode));
+        Argument.IsNotNull(nameof(start), start);
+        if (!ContainsNode(start)) throw new ArgumentException(nameof(start));
         var visited = Nodes.ToDictionary(n => n, _ => false, NodeEqualityComparer);
-        var path = DepthTraversalHelp(startNode, visited).ToArray();
+        var path = DepthTraversalHelp(start, visited).ToArray();
         return path;
     }
 
-    private IEnumerable<(TNode, TNode, TEdge)> DepthTraversalHelp(TNode node, 
+    private IEnumerable<ValueTuple<TNode, TNode, TEdge>> DepthTraversalHelp(TNode node, 
         Dictionary<TNode, bool> visited)
     {
         visited[node] = true;
@@ -138,6 +135,61 @@ public class Graph<TNode, TEdge>
             yield return (node, adjacent, Edge(node, adjacent));
             foreach (var tpl in DepthTraversalHelp(adjacent, visited)) yield return tpl;
         }
+    }
+
+    public ICollection<ValueTuple<TNode, TNode, TEdge>> BreadthTraversal(TNode start)
+    {
+        Argument.IsNotNull(nameof(start), start);
+        if (!ContainsNode(start)) throw new ArgumentException(nameof(start));
+
+        var queue = new Queue<TNode>();
+        var visited = Nodes.ToDictionary(n => n, _ => false, NodeEqualityComparer);
+        var path = new LinkedList<ValueTuple<TNode, TNode, TEdge>>();
+        
+        queue.Enqueue(start);
+
+        var predecessor = start;
+        while (queue.Any())
+        {
+            var node = queue.Dequeue();
+            var edge = Edge(predecessor, node);
+            path.AddLast((predecessor, node, edge));
+
+            visited[node] = true;
+
+            var neighbours = _adjacent[node];
+
+            foreach (var nbr in neighbours)
+            {
+                if (!visited[nbr]) queue.Enqueue(nbr);
+            }
+            predecessor = node;
+        }
+        return path;
+    }
+
+    public ICollection<Graph<TNode, TEdge>> Split()
+    {
+        if (EdgesCount == 0) throw new Exception("Graph is empty");
+        if (NodesCount == 0) throw new Exception("Graph is empty");
+
+        var ret = new LinkedList<Graph<TNode, TEdge>>();
+
+        var nodes = new HashSet<TNode>(Nodes, NodeEqualityComparer);
+        while (nodes.Any())
+        {
+            var start = nodes.First();
+            var bfs = BreadthTraversal(start);
+            var bfsNodes = bfs.Select(tpl => tpl.Item2).ToArray();
+            foreach (var node in bfsNodes)
+            {
+                nodes.Remove(node);
+            }
+            nodes.Remove(start);
+            var newGraph = new Graph<TNode, TEdge>(bfsNodes, n => _adjacent[n], n => _edges[n], Directed);
+            ret.AddLast(newGraph);
+        }
+        return ret;
     }
 
     public ICollection<TNode> Nodes => _adjacent.Keys;
