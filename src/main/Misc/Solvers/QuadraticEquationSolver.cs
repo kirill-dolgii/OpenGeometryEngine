8c94c48fa1c175477fe0d23cfe0eeb759f4286d8
WawyWaker <maindolgiykirill99@gmail.com>
2024-03-25 15:55:24 +0300

    Solver refactoring

--- src/main/Misc/Solvers/QuadraticEquationSolver.cs
+++ src/main/Misc/Solvers/QuadraticEquationSolver.cs
@@ -1,31 +1,44 @@
+using OpenGeometryEngine.Misc.Solvers;
 using System;
 using System.Numerics;
 
 namespace OpenGeometryEngine;
 
-public static class QuadraticEquationSolver
+public class QuadraticEquationSolver : SolverBase<Pair<Complex>>
 {
-    public static (Complex FirstRoot, Complex SecondRoot) Solve(double a, double b, double c)
+    public double a { get; }
+
+    public double b { get; }
+
+    public double c { get; }
+
+    public QuadraticEquationSolver(double a, double b, double c) 
+        => (this.a, this.b, this.c, Solved, Result) = (a, b, c, false, null);
+
+	public override Pair<Complex> Solve()
+	{
+        if (Solved && Result != null) return Result;
+
+		if (b == 0d)
+		{
+			var t = Complex.Sqrt(new Complex(-c / a, 0d));
+			Result = new (t, -t);
+		}
+
+		var q = -0.5 * (b + Math.Sign(b) * Complex.Sqrt(new Complex(b * b - 4 * a * c, 0d)));
+
+        Result = new(q / a, c / q);
+
+		return Result;
+	}
+
+	public bool TryGetRealRoots(out double root1, out double root2)
     {
-        if (b == 0d)
+        var roots = Solve();
+        if (roots.First.Imaginary == 0 && roots.Second.Imaginary == 0)
         {
-            var t = Complex.Sqrt(new Complex(-c / a, 0d));
-            return (t, -t);
-        }
-
-        var q = -0.5 * (b + Math.Sign(b) * Complex.Sqrt(new Complex(b * b - 4 * a * c, 0d)));
-
-        return (q / a, c / q);
-    }
-
-    public static bool TryGetRealRoots(double a, double b, double c,
-                                       out double root1, out double root2)
-    {
-        var roots = Solve(a, b, c);
-        if (roots.Item1.Imaginary == 0 && roots.Item2.Imaginary == 0)
-        {
-            root1 = roots.Item1.Real;
-            root2 = roots.Item2.Real;
+            root1 = roots.First.Real;
+            root2 = roots.Second.Real;
             return true;
         }
         else
